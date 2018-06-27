95bbe215325516ab26043e01c21443e0d5afb97e
andizq <andres.izquierdo@udea.edu.co>
2018-06-27 01:27:29 -0500

    modelled images

--- plot_fits_image.py
+++ plot_fits_image.py
@@ -1,14 +1,16 @@
 import radmc3dPy.image as image
 import matplotlib.pyplot as plt
 import numpy as np
+from argparse import ArgumentParser
 
-i = 7
+parser = ArgumentParser(prog='Burgers', description='Self-obscuration')
+parser.add_argument('-f', '--file', help='fits file to write')
+args = parser.parse_args()
+
+if args.file: file = args.file
+else: file = 'image.fits'
+    
 dist = 5410.
-#lambdas = np.array([ 37500., 20000., 13043.47826087, 9375., 7142.85714286, 
-#                     5454.54545455, 4285.71428571, 3529.41176471])
-#en GHz: np.array([8,15,23,32,42,55,70,85])*1. GHz.
-
-
 im = image.readImage()
 
 #image.plotImage(im, au=True, log=True, maxlog=10, saturate=1e-5, cmap=plt.cm.gist_heat)
@@ -18,7 +20,7 @@ image.plotImage(im, dpc=dist, arcsec=True, log=True, maxlog=10, saturate=1e-5, c
 cim = im.imConv(fwhm=[1.0, 0.6], pa=120., dpc=dist)
 image.plotImage(cim, arcsec=True, dpc=dist, log=True, maxlog=10, bunit='snu', cmap=plt.cm.gist_heat)
 """
-im.writeFits('img_shell.fits', dpc=5410.)#, coord='03h10m05s -10d05m30s')
+im.writeFits(file, dpc=dist)#, coord='03h10m05s -10d05m30s')
 
 
 #Mirar lo de inputs desde terminal
