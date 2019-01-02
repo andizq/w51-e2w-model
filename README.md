47c771e01c797bf283a443662b01157aea48f516
andizq <andres.izquierdo@udea.edu.co>
2019-01-02 16:51:16 +0000

    15 deg rotation along y-axis + README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # w51-e2w-model.
+
+Run everything via:
+
+```bash
+python generate_master.py
+```
+
+## Outputs:
+*	sed_all.png
+*	data/image_45ghz.fits
+*	data/image_95ghz.fits
+*	data/image_45ghz-conv.fits
+*	data/image_95ghz-conv.fits
+
+## External software:
+*       [radmc-3d](http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/)
+*	[sf3dmodels](https://star-forming-regions.readthedocs.io/en/latest/)
+* 	[astropy](http://www.astropy.org/)
--- exec_paraboloid.py
+++ exec_paraboloid.py
@@ -31,7 +31,7 @@ T0 = 10000.
 qT = 0.
 temp = [T0, qT]
 
-dens0 = 2.5e14 
+dens0 = 2.6e14 
 qn = -0.9 #-2
 #dens0 = 2e14 * 5 / 2.5 / 50
 #qn = -0.5 #-2
@@ -42,7 +42,7 @@ b = 2*np.sqrt(300*AU) #10*AU
 GRID, props  = make_paraboloid(z_min, z_max, drBIGGRID, a, b, dens, T0)
 shift = Model.ChangeGeometry(GRID, center = np.array([0, dy, dx])*AU,
                              rot_dict = { 'angles': [-np.pi/180*(posang+90 + 8),
-                                                      -np.pi/6], 
+                                                      -np.pi/12], 
                                           'axis': ['x','y'] })
 GRID.XYZ = shift.newXYZ
 Make_Datatab(props, GRID).submodel(tag = tag)
